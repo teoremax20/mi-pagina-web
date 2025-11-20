f296e218c80f861f0b61fa5f7f04ab5f62f54f5e
Miguel Serna <teoremax20@gmail.com>
2025-11-20 11:36:40 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("formulario-contacto").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Gracias por tu mensaje. Me pondré en contacto contigo pronto.");
+});
